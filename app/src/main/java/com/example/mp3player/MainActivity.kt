9c9568969b7e5b5a061f251dd501c5977ca8adbd
Pratik Ranjan <pratikranjaniitg101@gmail.com>
2023-06-20 09:31:54 +0530

    #comment databinding added

---
+++ app/src/main/java/com/example/mp3player/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mp3player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mp3player.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+
+    }
+
+
+}
