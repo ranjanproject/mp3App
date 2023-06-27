e94bc0548c9c5bc4caa805c3748ce22ee8640e16
Pratik Ranjan <pratikranjaniitg101@gmail.com>
2023-06-27 09:10:39 +0530

    mp3 added in forground services

--- app/src/main/java/com/example/mp3player/MP3ForegroundService.kt
+++ app/src/main/java/com/example/mp3player/MP3ForegroundService.kt
@@ -1,17 +1,57 @@
 package com.example.mp3player
 
+import android.R
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.IBinder
+import android.provider.Settings
+
 
 class MP3ForegroundService: Service() {
+    lateinit var mediaPlayer: MediaPlayer
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        mediaPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI)
+        mediaPlayer.isLooping = true
+
+        mediaPlayer.start()
+
+        val CHANNELID = "Foreground Service ID"
+        val channel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel(
+                CHANNELID,
+                CHANNELID,
+                NotificationManager.IMPORTANCE_LOW
+            )
+        } else {
+//            TODO("VERSION.SDK_INT < O")
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            getSystemService(NotificationManager::class.java).createNotificationChannel(channel as NotificationChannel)
+        }
+        val notification: Notification.Builder  = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Notification.Builder(this, CHANNELID)
+                .setContentText("Service is running")
+                .setContentTitle("Service enabled")
+                .setSmallIcon(R.drawable.btn_dialog)
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+
+        startForeground(1001, notification.build())
 
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        mediaPlayer.stop()
     }
     override fun onBind(p0: Intent?): IBinder? {
 
--- app/src/main/java/com/example/mp3player/MainActivity.kt
+++ app/src/main/java/com/example/mp3player/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mp3player
 
+import android.app.ActivityManager
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mp3player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,14 +22,42 @@ class MainActivity : AppCompatActivity() {
 
     private fun initClickListeners() {
         binding.startBtn.setOnClickListener {
-            startService(Intent(this, MP3Service::class.java))
+//            startService(Intent(this, MP3Service::class.java))
 //            Toast.makeText(this, "start Clicked", Toast.LENGTH_LONG).show()
+            if (!checkForGroundServiceIsRunning()) {
+                val serviceIntent = Intent(
+                    this,
+                    MP3ForegroundService::class.java
+                )
+                //check for older version
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(serviceIntent)
+                }
+            }
         }
 
         binding.stopBtn.setOnClickListener {
-            stopService(Intent(this, MP3Service::class.java))
-//            Toast.makeText(this, "stop Clicked", Toast.LENGTH_LONG).show()
+            stopService(Intent(this, MP3ForegroundService::class.java))
+//            Toast.makeText(this, "stop Cl`icked", Toast.LENGTH_LONG).show()
         }
+    }
+
+
+    /**
+     * This method will check if any service
+     * is already running or not
+     * Check will help to avoid any repeated service start
+     */
+    private fun checkForGroundServiceIsRunning(): Boolean{
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+        for(service in activityManager.getRunningServices(Int.MAX_VALUE)){
+            if (MP3ForegroundService::class.java.name == service.service.className) {
+                return true
+            }
+        }
+
+        return false
     }
 
 
